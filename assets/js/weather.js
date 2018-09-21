7f71e37c24296eed9439da62cc8ed0eaa2d9cc4c
Ahil-kanna <g.ahil1998@gmail.com>
2018-09-21 22:12:39 +0530

    calendar added

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -53,8 +53,8 @@ function httpGetAsync(url, callback) {
 
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function () {
-    if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-    callback(JSON.parse(xmlHttp.responseText));
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(JSON.parse(xmlHttp.responseText));
     };
     xmlHttp.open("GET", url, true);
     xmlHttp.send();
