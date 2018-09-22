745e8b732b13cf1f41579a2cfd52217d2fe0b2d8
Ahil-kanna <g.ahil1998@gmail.com>
2018-09-22 22:17:06 +0530

    version 1.0

---
+++ Weather/assets/js/weatherData.js
@@ -0,0 +1,44 @@
+var weatherData = {
+    "coord": {
+        "lon": -114.09,
+        "lat": 51.05
+    },
+    "weather": [
+        {
+            "id": 500,
+            "main": "Rain",
+            "description": "light rain",
+            "icon": "10n"
+        }
+    ],
+    "base": "cmc stations",
+    "main": {
+        "temp": 20.839,
+        "pressure": 871.33,
+        "humidity": 88,
+        "temp_min": 3.839,
+        "temp_max": 40.839,
+        "sea_level": 1032.57,
+        "grnd_level": 871.33
+    },
+    "wind": {
+        "speed": 2.07,
+        "deg": 337.501
+    },
+    "rain": {
+        "3h": 0.87
+    },
+    "clouds": {
+        "all": 92
+    },
+    "dt": 1462506236,
+    "sys": {
+        "message": 0.011,
+        "country": "CA",
+        "sunrise": 1462535959,
+        "sunset": 1462590452
+    },
+    "id": 5913490,
+    "name": "Calgary",
+    "cod": 200
+};
